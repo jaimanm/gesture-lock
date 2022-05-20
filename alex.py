1a6411f03e84381a8b3a11d76894360dbaa52704
jaimanm <jaimancoding@gmail.com>
2022-05-20 11:25:41 -0400

    alex pls

--- alex.py
+++ alex.py
@@ -1,68 +1,106 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-
-# For webcam input:
+import time
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+pw = [0, 1, 2, 3, 17]
+locked = True
+def getInput(num):
+  num = int(num)
+  inputPw = []
+  mp_drawing = mp.solutions.drawing_utils
+  mp_drawing_styles = mp.solutions.drawing_styles
+  mp_hands = mp.solutions.hands
+  # For webcam input:
+  for i in range(num) :
+    print("Prepare Gesture " + str(i + 1))
+    time.sleep(1)
+    count = 0
+    anotherList = []
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+      while cap.isOpened():
+        
+        success, image = cap.read()
+        if not success:
+          print("Ignoring empty camera frame.")
+          # If loading a video, use 'break' instead of 'continue'.
+          continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    temp2 = []
-    
-    if results.multi_hand_landmarks:
-      for hand_no, hand_landmarks in enumerate(results.multi_hand_landmarks):
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-        list = []
-        for i in range(21):
-          temp = []
-          temp.append(hand_no)
-          temp.append(i)
-          temp.append(int(hand_landmarks.landmark[mp_hands.HandLandmark(i).value].x * cap.get(3)))
-          temp.append(int(hand_landmarks.landmark[mp_hands.HandLandmark(i).value].y * cap.get(4)))
-          temp.append(int(hand_landmarks.landmark[mp_hands.HandLandmark(i).value].z * -100))
-          list.append(temp)
-        temp2 = list
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        temp2 = []
+        
+        if results.multi_hand_landmarks:
+          for hand_no, hand_landmarks in enumerate(results.multi_hand_landmarks):
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+            list = []
+            for i in range(21):
+              temp = []
+              temp.append(hand_no)
+              temp.append(i)
+              temp.append(int(hand_landmarks.landmark[mp_hands.HandLandmark(i).value].x * cap.get(3)))
+              temp.append(int(hand_landmarks.landmark[mp_hands.HandLandmark(i).value].y * cap.get(4)))
+              temp.append(int(hand_landmarks.landmark[mp_hands.HandLandmark(i).value].z * -100))
+              list.append(temp)
+            temp2 = list
 
 
-
-        #identify gesture
-        str = ""
-        for i in range(5) :
-          dist1 = np.hypot(list[(i + 1) * 4][2] - list[0][2], list[((i + 1) * 4)][3] - list[0][3])
-          dist2 = np.hypot(list[(i + 1) * 4 - 1][2] - list[0][2], list[((i + 1) * 4 - 1)][3] - list[0][3])
-          if dist1 > dist2 :
-            str = str + "1"
+            #todo: create average of palm points to accurately depict thumb distance
+            #identify gesture
+            s = ""
+            for i in range(5) :
+              averagePalmX = (list[0][2] + list[5][2] + list[13][2] + list[17][2]) / 4
+              averagePalmY = (list[0][3] + list[5][3] + list[13][3] + list[17][3]) / 4
+              dist1 = np.hypot(list[(i + 1) * 4][2] - averagePalmX, list[((i + 1) * 4)][3] - averagePalmY)
+              dist2 = np.hypot(list[(i + 1) * 4 - 2][2] - averagePalmX, list[((i + 1) * 4 - 2)][3] - averagePalmY)
+              dist3 = np.hypot(list[(i + 1) * 4 - 1][2] - averagePalmX, list[((i + 1) * 4 - 1)][3] - averagePalmY)
+              dist4 = np.hypot(list[(i + 1) * 4 - 3][2] - averagePalmX, list[((i + 1) * 4 - 3)][3] - averagePalmY)
+              if dist1 < dist2 or dist1 < dist3 or dist1 < dist4:
+                s = s + "0"
+              else :
+                s = s + "1"
+            dec_number= int(s[::-1], 2)
+            anotherList.append(dec_number)
+            blank = str(hand_no + 1)
+            blank2 = str(dec_number)
+            #print("Hand number: " + blank + " : " + blank2)
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        if cv2.waitKey(5) == ord('q'):
+          break
+        if count >= 50:
+          if len(anotherList) > 0:
+            inputPw.append(max(set(anotherList), key = anotherList.count))
           else :
-            str = str + "0"
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) == ord('q'):
-      break
-    #count += 1 
+            inputPw.append(-1)
+          break
+        count += 1 
+  print(inputPw)
+  return inputPw
+def checkPw():
+  if locked == True :
+    if pw == getInput(len(pw)) :
+      locked = False
+  
+def setPw() :
+  if locked == False :
+    x = getInput(1)[0]
+    if not x == 0 : 
+      pw = getInput(getInput(1)[0])
+print(checkPw())
 cap.release()
