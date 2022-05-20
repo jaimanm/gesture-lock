c113f5d75e06b45416c0cad6449988b61418d497
jaimanm <jaimancoding@gmail.com>
2022-05-20 13:53:21 -0400

    changed count check from 50 to 30

--- alex.py
+++ alex.py
@@ -81,7 +81,7 @@ def getInput(num):
           anotherList.append(-1)
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-        if count >= 50:
+        if count >= 30:
           mode = max(set(anotherList), key = anotherList.count)
           inputPw.append(mode)
           print(mode)
